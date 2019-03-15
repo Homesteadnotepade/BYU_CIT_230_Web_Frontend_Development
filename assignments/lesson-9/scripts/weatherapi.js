9e0c5d3cac9441c4113b5c1d5c3bf1165746aee9
jaypstay <jaypstay@byui.edu>
2019-03-14 19:24:14 -0700

    week 10 folder added

--- assignments/lesson-9/scripts/weatherapi.js
+++ assignments/lesson-9/scripts/weatherapi.js
@@ -1,2 +1,17 @@
 let weatherRequest = new XMLHttpRequest();
-api.openweathermap.org/data/2.5/forecast?id=524901&APPID=8ba9cf20950d82df6a347fc632349c65
+let apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=8ba9cf20950d82df6a347fc632349c65';
+weatherRequest.open('GET', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload =  function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+    document.getElementById('current-temp').innerHTML = weatherData.main.temp;
+
+    let icon = "http://openweathermap.org/img/w/" + weatherData.weater[0].icon + ".png";
+    let desc = weatherData.weather.description;
+
+    document.getElementById('current-temp').setAttribute('src', icon);
+    document.getElementById('current-temp').setAttribute('alt', desc);
+}
