96d1bf92ee180b139d13bc520aaa7b5daeb1f750
jaypstay <jaypstay@byui.edu>
2019-04-04 20:23:53 -0700

    Update for new Term Project

---
+++ assignments/term-project/scripts/templeINFO.js
@@ -0,0 +1,121 @@
+"temple": [
+    {
+        "templeName": "Mesa Arizona Temple",
+        "address": "101 South LeSueur, Mesa, Arizona  85204-1031",
+        "telephone": "480-833-1211",
+        "email": "mesatemple@temple.com",
+        "services": [
+            "Clothing rental available",
+            "Cafeteria available",
+            "NO patron housing available",
+            "Distribution center nearby"
+            ]
+        "history": [
+            "Announcement": "3 October 1919",
+            "Site Dedication": "28 November 1921 by Heber J. Grant",
+            "Groundbreaking": "25 April 1922 by Heber J. Grant",
+            "Dedication": "23–26 October 1927 by Heber J. Grant"
+            ]
+        "ordinance schedule": "Temple closed during renovation",
+        "session schedule": "Temple closed during renovation",
+        "temple closure schedule": "Temple closed during renovation",
+    },
+    {
+        "templeName": "Phoenix Arizona Temple",
+        "address": "5220 West Pinnacle Peak Road, Phoenix, Arizona  85310",
+        "telephone": "623-474-9500",
+        "email": "phoenixtemple@temple.com",
+        "services": [
+            "NO clothing rental available",
+            "NO cafeteria available",
+            "NO patron housing available",
+            "NO distribution center nearby",
+        ]
+        "history": [
+            "Announcement": "May 24, 2008",
+            "Site Dedication": "June 4, 2011 by Ronald A. Rasband",
+            "Groundbreaking": "June 4, 2011 by Ronald A. Rasband",
+            "Dedication": "November 16, 2014 by Thomas S. Monson"
+        ]
+        "ordinance schedule": [
+            "Baptism": [
+                "Tuesday - Saturday: 7:00 AM - 11:30 AM Walk-ins and Appointments",
+                "Tuesday - Saturday: 4:00 PM - 8:00 PM Appointments Only",
+            ]
+            "Initiatory": [
+                "Tuesday - Saturday: 7:00 AM - 11:30 Walk-ins Only",
+                "Tuesday - Saturday: 4:00 PM - 8:00 PM Walk-ins Only",
+            ]
+            "Sealing": [
+                "Tuesday - Saturday: 7:00 AM - 11:30 Walk-ins and Appointments",
+                "Tuesday - Saturday: 4:00 PM - 8:00 PM Walk-ins and Appointments",
+            ]
+            
+        ]
+        "session schedule": [
+            "Endowment": [
+                "Tuesday - Saturday": [
+                    "7:30 AM Walk-ins Only",
+                    "8:30 AM Walk-ins Only",
+                    "9:30 AM Walk-ins Only",
+                    "10:30 AM Walk-ins Only",
+                    "4:30 PM Walk-ins and Appointments",
+                    "5:30 PM Walk-ins Only",
+                    "6:30 PM Walk-ins Only",
+                    "7:30 PM Walk-ins Only - Spanish",
+                ]
+                "Sunday - Monday": [
+                    "Closed"
+                ]
+            ]
+        ]
+        "temple closure schedule": [
+            "Saturday, 6 April 2019",
+            "Monday, 13 May 2019 - Monday, 27 May 2019",
+            "Saturday, 5 October 2019",
+            "Wednesday, 27 November 2019 (Limited Hours)",
+            "Thursday, 28 November 2019",
+            "Monday, 2 December 2019 - Monday, 16 December 2019",
+            "Tuesday, 24 December 2019 - Wednesday, 25 December 2019",
+        ]
+    },
+    {
+        "templeName": "Snowflake Arizona Temple",
+        "address": "1875 West Canyon Drive, Snowflake, Arizona  85937",
+        "telephone": "928-536-6626",
+        "email": "snowflaketemple@temple.com",
+        "services": [
+            "NO clothing rental available"
+            "NO cafeteria available"
+            "NO patron housing available"
+            "Distribution center nearby"
+        ]
+        "history": [
+            "Announcement": "April 2, 2000",
+            "Groundbreaking": "September 23, 2000 by Rex D. Pinegar",
+            "Site Dedication": "September 23, 2000 by Rex D. Pinegar",
+            "Dedication": "March 3, 2002 by Gordon B. Hinckley",
+        ]
+        "ordinance schedule": "",
+        "session schedule": "",
+        "temple closure schedule": [
+            "Monday, 1 April 2019 - Monday, 15 April 2019",
+            "Thursday, 4 July 2019",
+            "Monday, 30 September 2019 - Monday, 14 October 2019",
+            "Wednesday, 27 November 2019 - Thursday, 28 November 2019",
+            "Tuesday, 24 December 2019 - Wednesday, 25 December 2019",
+            "Tuesday, 31 December 2019 - Wednesday, 1 January 2020",
+        ]
+    },
+    {
+        "templeName": "",
+        "address": "",
+        "telephone": "",
+        "email": "",
+        "services": "",
+        "history": "",
+        "ordinance schedule": "",
+        "session schedule": "",
+        "temple closure schedule": "",
+        ]
+    }
