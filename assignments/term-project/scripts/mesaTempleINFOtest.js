393dc47f7b8be1c35793e692154d86658c58bbd9
jaypstay <jaypstay@byui.edu>
2019-04-12 15:55:18 -0700

    update

--- assignments/term-project/scripts/mesaTempleINFOtest.js
+++ assignments/term-project/scripts/mesaTempleINFOtest.js
@@ -13,16 +13,39 @@ mesaRequest.onload = function() {
 };
 
 function populateMesa(jsonObj){
-    var temple = jsonObj.temple;
+    var temple = jsonObj.templeService;
 
-    for (var i = 0; i < temple; i++){
-       var temtempleNameplename = temple[i].name;
-        if (templeName === "Mesa Arizona Temple"){
+    for (var i = 0; i < templeService; i++){
+       var templeName = templeService[i].name;
+        if (templeService[i].name === "Mesa Arizona Temple"){
           
             var services = document.createElement("h2");
-            services.textContent = "services:" + temple.services;
+            services.textContent = "Services: " + templeService.services;
           
           document.getElementById("MesaTempleInfo").appendChild(services);
         }
     }
 }
+var mesaJsonURL = 'https://jaypstay.github.io/assignments/term-project/scripts/templeService.json';
+ 
+var mesaRequest = new XMLHttpRequest();
+
+mesaRequest.open('GET', mesaJsonURL);
+
+mesaRequest.responseType = 'json';
+mesaRequest.send();
+
+mesaRequest.onload = function() {
+    var mesaInfo = mesaRequest.response;
+    populateMesa(mesaInfo);
+
+};
+
+function populateMesa(jsonObj){
+    var templeService = jsonObj.templeService;
+
+    for (var i = 0; i < templeService.length; i++){
+       var service = templeService[i].services;
+        if (service === "Mesa Arizona Temple"){
+            var townEvent0 = document.createElement("h3");
+            townEvent0.textContent += towns[i].events[0];
